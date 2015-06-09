3c3eb67feb1bdca561831468cffbbebd2609b9e7
Natasha Scott <namscott@gmail.com>
2015-06-09 15:45:49 -0400

    added feature sections to homepage in a static manner to create styles, will be replaced by variables

---
+++ core/static/js/opencanada.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+
+    $('.fa-camera').click(function(){
+        $('.feature-text').fadeToggle();
+        $('.feature-image-overlay').fadeToggle();
+    });
+
+
+});
