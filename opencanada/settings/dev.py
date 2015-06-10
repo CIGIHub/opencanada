1bd9d1d267c5eb21b339e20379d6c05e56dc65fc
Caroline Simpson <csimpson@cigionline.org>
2015-06-10 13:47:02 -0400

    Adding development settings for wordpress importer.

--- opencanada/settings/dev.py
+++ opencanada/settings/dev.py
@@ -4,3 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WP_IMPORTER_IMAGE_DOWNLOAD_DOMAINS = ('opencanada.centos', 'opencanada.org',
+                                      'www.opencanada.org')
+WP_IMPORTER_USER_PHOTO_URL_PATTERN = "http://opencanada.centos/wp-content/uploads/userphoto/{}"
