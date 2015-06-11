b639018956a99af70050fa7de0b945adbc34278a
Natasha Scott <namscott@gmail.com>
2015-06-11 09:17:07 -0400

    added scrolling to landing page image

--- core/static/js/opencanada.js
+++ core/static/js/opencanada.js
@@ -5,5 +5,29 @@ jQuery(document).ready(function($){
         $('.feature-image-overlay').fadeToggle();
     });
 
+    //$('a[href^="#feature"]').on('click',function (e) {
+	//    e.preventDefault();
+    //
+	//    var target = this.hash;
+	//    var $target = $(target);
+    //
+	//    $('html, body').stop().animate({
+	//        'scrollTop': $target.offset().top
+	//    }, 900, 'swing', function () {
+	//        window.location.hash = target;
+	//    });
+	//});
 
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
