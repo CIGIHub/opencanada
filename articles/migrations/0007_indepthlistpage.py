64243b3c0750d2365ced7ffed061a432fc0a8f30
Caroline Simpson <csimpson@cigionline.org>
2015-06-18 14:33:39 -0400

    OCW-2 #comment Created initial models for InDepth pages. #time 2h

---
+++ articles/migrations/0007_indepthlistpage.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '0001_squashed_0016_change_page_url_path_to_text_field'),
+        ('articles', '0006_auto_20150618_1750'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='InDepthListPage',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+    ]
--- articles/models.py
+++ articles/models.py
@@ -2,9 +2,13 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.wagtailadmin.edit_handlers import (FieldPanel, InlinePanel,
+                                                PageChooserPanel,
+                                                StreamFieldPanel)
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 
 from people import models as people_models
@@ -14,6 +18,8 @@ from . import fields as article_fields
 
 @python_2_unicode_compatible
 class ArticleListPage(Page):
+    subpage_types = ['ArticlePage']
+
     def __str__(self):
         return self.title
 
@@ -48,47 +54,56 @@ ArticlePage.content_panels = Page.content_panels + [
     StreamFieldPanel('body'),
 ]
 
-# class InDepthArticleLink(models.Model):
-#     image = models.ForeignKey(
-#         'wagtailimages.Image',
-#         null=True,
-#         blank=True,
-#         on_delete=models.SET_NULL,
-#         related_name='+'
-#     )
-#     text = RichTextField()
-#     article = models.ForeignKey(
-#         "ArticlePage",
-#         null=True,
-#         blank=True,
-#         on_delete=models.SET_NULL,
-#         related_name='+'
-#     )
-#     in_depth = ParentalKey(
-#         "InDepthPage",
-#         related_name='related_articles'
-#     )
-#
-#     panels = [
-#         FieldPanel("text"),
-#         ImageChooserPanel("image"),
-#         PageChooserPanel("article", 'core.ArticlePage')
-#     ]
 
-#
-# class InDepthPage(Page):
-#     intro = RichTextField(blank=True, default="")
-#     image = models.ForeignKey(
-#         'wagtailimages.Image',
-#         null=True,
-#         blank=True,
-#         on_delete=models.SET_NULL,
-#         related_name='+'
-#     )
-#
-#
-# InDepthPage.content_panels = [
-#     RichTextFieldPanel('intro'),
-#     ImageChooserPanel('image'),
-#     InlinePanel('related_articles')
-# ]
+@python_2_unicode_compatible
+class InDepthListPage(Page):
+    subpage_types = ['InDepthPage']
+
+    def __str__(self):
+        return self.title
+
+
+class InDepthArticleLink(models.Model):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    text = RichTextField()
+    article = models.ForeignKey(
+        "ArticlePage",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    in_depth = ParentalKey(
+        "InDepthPage",
+        related_name='related_articles'
+    )
+
+    panels = [
+        FieldPanel("text"),
+        ImageChooserPanel("image"),
+        PageChooserPanel("article", 'articles.ArticlePage')
+    ]
+
+
+class InDepthPage(Page):
+    body = article_fields.BodyField(blank=True, default="")
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+
+InDepthPage.content_panels = Page.content_panels + [
+    StreamFieldPanel('body'),
+    ImageChooserPanel('image'),
+    InlinePanel('related_articles', label="Articles")
+]
