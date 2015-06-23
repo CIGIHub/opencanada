e87c93287ee33d4384916d3e103bcf40000bbf55
Caroline Simpson <csimpson@cigionline.org>
2015-06-23 10:52:20 -0400

    Adding limitation to only allow adding list pages to home currently.

--- core/models.py
+++ core/models.py
@@ -5,11 +5,17 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel, RichTextFieldPanel
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailsnippets.models import register_snippet
 
+from articles import models as article_models
 from people import models as people_models
 
 
 @python_2_unicode_compatible
 class HomePage(Page):
+    sub_types = [
+        article_models.ArticleListPage,
+        article_models.InDepthListPage
+    ]
+
     def __str__(self):
         return self.title
 
