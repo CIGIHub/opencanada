4b30bbcde1ae9cdb3b8fda242e32d44025ef1e0a
Caroline Simpson <csimpson@cigionline.org>
2015-06-23 11:41:00 -0400

    Update the contenttypes before trying to access them.

--- articles/migrations/0010_create_indepth_page.py
+++ articles/migrations/0010_create_indepth_page.py
@@ -2,21 +2,18 @@
 from __future__ import unicode_literals
 
 from django import VERSION as DJANGO_VERSION
+from django.contrib.contenttypes.management import update_contenttypes
 from django.db import migrations
 
 
 def create_indepth_page(apps, schema_editor):
+    update_contenttypes(apps.app_configs['articles'], interactive=False)
     Page = apps.get_model("wagtailcore", "Page")
     InDepthListPage = apps.get_model("articles", "InDepthListPage")
     home_page = Page.objects.get(slug="home")
     ContentType = apps.get_model("contenttypes", "ContentType")
-    # indepth_list_page_content_type = ContentType.objects.get_for_model(InDepthListPage)
+    indepth_list_page_content_type = ContentType.objects.get_for_model(InDepthListPage)
 
-    indepth_list_page_content_type, created = ContentType.objects.get_or_create(
-        model='indepthlistpage',
-        app_label='articles',
-        defaults={'name': 'indepthlistpage'} if DJANGO_VERSION < (1, 8) else {}
-    )
     # Create features page
     indepth_page = InDepthListPage.objects.create(
         title="InDepth",
