e5f00e55a7de72d68c957d14dd17194e4851bf57
Caroline Simpson <csimpson@cigionline.org>
2015-06-23 11:31:57 -0400

    Changing content type loading code in hopes of fixing failure on Travis.

--- articles/migrations/0010_create_indepth_page.py
+++ articles/migrations/0010_create_indepth_page.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django import VERSION as DJANGO_VERSION
+from django.db import migrations
 
 
-def create_features_page(apps, schema_editor):
+def create_indepth_page(apps, schema_editor):
     Page = apps.get_model("wagtailcore", "Page")
     InDepthListPage = apps.get_model("articles", "InDepthListPage")
     home_page = Page.objects.get(slug="home")
     ContentType = apps.get_model("contenttypes", "ContentType")
-    indepth_list_page_content_type = ContentType.objects.get_for_model(InDepthListPage)
+    # indepth_list_page_content_type = ContentType.objects.get_for_model(InDepthListPage)
 
+    indepth_list_page_content_type, created = ContentType.objects.get_or_create(
+        model='indepthlistpage',
+        app_label='articles',
+        defaults={'name': 'indepthlistpage'} if DJANGO_VERSION < (1, 8) else {}
+    )
     # Create features page
     indepth_page = InDepthListPage.objects.create(
         title="InDepth",
@@ -29,11 +35,11 @@ def create_features_page(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('articles', '0009_auto_20150619_2156'),
         ('contenttypes', '__latest__'),
         ('core', '__latest__'),
-        ('articles', '0009_auto_20150619_2156'),
     ]
 
     operations = [
-        migrations.RunPython(create_features_page),
+        migrations.RunPython(create_indepth_page),
     ]
