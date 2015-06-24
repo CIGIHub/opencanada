36d5e94556a89fb96f6f45a05699bc4dfc195c85
Caroline Simpson <csimpson@cigionline.org>
2015-06-24 18:31:03 -0400

    OCW-22 #time 10m Adding FontStyle snippet which can be selected for the homepage feature item.

--- core/models.py
+++ core/models.py
@@ -5,10 +5,32 @@ from django.utils.encoding import python_2_unicode_compatible
 from wagtail.wagtailadmin.edit_handlers import (FieldPanel, PageChooserPanel,
                                                 RichTextFieldPanel)
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 from wagtail.wagtailsnippets.models import register_snippet
 
 from articles import models as article_models
 from people import models as people_models
+
+
+@python_2_unicode_compatible
+class FontStyle(models.Model):
+    name = models.CharField(max_length=1024)
+    font_size = models.FloatField(default=1, help_text="The size of the fonts in ems.")
+    line_size = models.FloatField(default=100, help_text="The line height as a percentage.")
+    text_colour = models.CharField(max_length=64, default="#000000", help_text="The colour of the text in hexidecimal. For example, to get black enter '#000000'.")
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('font_size'),
+        FieldPanel('line_size'),
+        FieldPanel('text_colour'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+
+register_snippet(FontStyle)
 
 
 @python_2_unicode_compatible
@@ -26,11 +48,20 @@ class HomePage(Page):
         related_name='+'
     )
 
+    featured_item_font_style = models.ForeignKey(
+        'core.FontStyle',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     def __str__(self):
         return self.title
 
 HomePage.content_panels = Page.content_panels + [
     PageChooserPanel("featured_item", "articles.ArticlePage"),
+    SnippetChooserPanel("featured_item_font_style", FontStyle),
 ]
 
 
