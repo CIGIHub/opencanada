f7be43620a290a900b3ac7046ace4f3a833180f7
Caroline Simpson <csimpson@cigionline.org>
2015-07-09 14:46:04 -0400

    Adding test for new tag import model. Fixing str function.

--- wordpress_importer/tests/test_models.py
+++ wordpress_importer/tests/test_models.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import, unicode_literals
 
 from django.test import TestCase
 
-from wordpress_importer.models import ImageImport, PostImport
+from articles.models import Topic
+from wordpress_importer.models import ImageImport, PostImport, TagImport
 
 
 class PostImportTestCast(TestCase):
@@ -17,3 +18,11 @@ class ImageImportTestCase(TestCase):
         image_import = ImageImport(original_url="http://example.com/cat.jpg", name="cat.jpg")
 
         self.assertEqual(str(image_import), "cat.jpg")
+
+
+class TagImportTestCase(TestCase):
+    def test_str_returns_slug_tag_name(self):
+        topic, created = Topic.objects.get_or_create(name="Topic 1")
+        tag_import = TagImport(original_slug="my-slug", topic=topic)
+
+        self.assertEqual(str(tag_import), "my-slug - Topic 1")
