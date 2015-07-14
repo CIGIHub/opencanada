51e872f6798147df496053bed67ab46d8304e33a
Caroline Simpson <csimpson@cigionline.org>
2015-07-14 10:48:55 -0400

    Adding new models to django admin ui.

--- wordpress_importer/admin.py
+++ wordpress_importer/admin.py
@@ -5,3 +5,5 @@ from . import models
 # Register your models here.
 admin.site.register(models.ImageImport)
 admin.site.register(models.PostImport)
+admin.site.register(models.ImportDownloadError)
+admin.site.register(models.TagImport)
