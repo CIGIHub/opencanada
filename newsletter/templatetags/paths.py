c622e2bc9cf52f47ed8ce1c034fb951cdbcc0acb
Natasha Scott <namscott@gmail.com>
2015-08-07 09:14:46 -0400

    OCW-104 #comment adjusting line heights

--- newsletter/templatetags/paths.py
+++ newsletter/templatetags/paths.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.filter
 def paragraph_markup(text):
-    new_text = re.sub(r'<p[^>]*>', '<p style=\"-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: \'Droid Serif\', Georgia, serif; font-size: 16px; line-height: 120%\">', text, 0)
+    new_text = re.sub(r'<p[^>]*>', '<p style=\"-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; margin: 5px 0; font-family: \'Droid Serif\', Georgia, serif; font-size: 16px; line-height: 120%;\">', text, 0)
 
     return new_text
 
