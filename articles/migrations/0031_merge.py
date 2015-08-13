aa25ad3bd559c759b616eb84ae422c7794722d4d
Caroline Simpson <csimpson@cigionline.org>
2015-08-13 11:33:16 -0400

    Adding migration merge.

---
+++ articles/migrations/0031_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('articles', '0030_auto_20150806_2136'),
+        ('articles', '0030_articlecategory_include_main_image'),
+    ]
+
+    operations = [
+    ]
