5cfdd3a0b8762e123109cbcd33c860eba455df70
Caroline Simpson <csimpson@cigionline.org>
2015-08-13 21:31:38 -0400

    Fixing contributor articles on contributor pages.

--- people/templatetags/people_tags.py
+++ people/templatetags/people_tags.py
@@ -6,8 +6,11 @@ register = template.Library()
 
 
 @register.assignment_tag()
-def contributor_articles(contributor_page, article_page_id, number=None):
-    articles = ArticlePage.objects.filter(author_links__author=contributor_page).exclude(id=article_page_id)
+def contributor_articles(contributor_page, article_page_id=None, number=None):
+    if article_page_id:
+        articles = ArticlePage.objects.filter(author_links__author=contributor_page).exclude(id=article_page_id)
+    else:
+        articles = ArticlePage.objects.filter(author_links__author=contributor_page)
     if number:
         articles = articles[:number]
     return articles
