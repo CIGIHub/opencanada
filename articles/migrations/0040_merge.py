ffd0fcb8b6e1ae028c81c34d0bd6d59e3f966a5f
Natasha Scott <namscott@gmail.com>
2015-08-14 15:48:44 -0400

    adding merge migration

---
+++ articles/migrations/0040_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('articles', '0039_auto_20150814_1922'),
+        ('articles', '0038_auto_20150814_1826'),
+    ]
+
+    operations = [
+    ]
