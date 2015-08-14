04d20f670270d7169e8e102d79c8ed2e1e446d06
Caroline Simpson <csimpson@cigionline.org>
2015-08-14 14:31:53 -0400

    OCW-221 Moved flag to hide main image on article pages to the article page itself rather than the category.  Renamed the author blurb flag to eliminate the double negative and make it consistent with the image flag.

--- articles/models.py
+++ articles/models.py
@@ -169,7 +169,6 @@ class ArticleCategory(UniquelySlugable):
     objects = ArticleCategoryManager()
 
     name = models.CharField(max_length=1024)
-    include_main_image = models.BooleanField(default=True)
 
     class Meta:
         verbose_name_plural = "Article Categories"
@@ -304,9 +303,8 @@ class ArticlePage(Page, FeatureStyleFields, Promotable, Sharelinks):
         null=True
     )
 
-    hide_author_block = models.BooleanField(
-        default=False
-    )
+    include_author_block = models.BooleanField(default=True)
+    include_main_image = models.BooleanField(default=True)
 
     search_fields = Page.search_fields + (
         index.SearchField('excerpt', partial_match=True),
@@ -415,7 +413,8 @@ class ArticlePage(Page, FeatureStyleFields, Promotable, Sharelinks):
     style_panels = [
         MultiFieldPanel(
             [
-                FieldPanel('hide_author_block'),
+                FieldPanel('include_main_image'),
+                FieldPanel('include_author_block'),
             ],
             heading="Sections"
         )
