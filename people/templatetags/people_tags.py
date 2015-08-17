e67936dc5aed4170332f48aaf15fdfc349b31d51
Caroline Simpson <csimpson@cigionline.org>
2015-08-17 13:29:11 -0400

    Adding ordering to lists of contributor's articles.

--- people/templatetags/people_tags.py
+++ people/templatetags/people_tags.py
@@ -8,9 +8,9 @@ register = template.Library()
 @register.assignment_tag()
 def contributor_articles(contributor_page, article_page_id=None, number=None):
     if article_page_id:
-        articles = ArticlePage.objects.live().filter(author_links__author=contributor_page).exclude(id=article_page_id)
+        articles = ArticlePage.objects.live().filter(author_links__author=contributor_page).exclude(id=article_page_id).order_by("-first_published_at")
     else:
-        articles = ArticlePage.objects.live().filter(author_links__author=contributor_page)
+        articles = ArticlePage.objects.live().filter(author_links__author=contributor_page).order_by("-first_published_at")
     if number:
         articles = articles[:number]
     return articles
