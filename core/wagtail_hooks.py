0411202f8863ea3b37ac7c01a7e6b55487fe6cca
Caroline Simpson <csimpson@cigionline.org>
2015-08-18 16:41:10 -0400

    OCW-195 #time 2h Added superscript and subscript options to the wagtail richtext field editor.

--- core/wagtail_hooks.py
+++ core/wagtail_hooks.py
@@ -1,21 +1,40 @@
 from __future__ import absolute_import
 
+from django.conf import settings
+from django.utils.html import format_html, format_html_join
 from wagtail.wagtailcore import hooks
 
 
 @hooks.register('insert_editor_js')
 def editor_js():
-    return """
+
+    js_files = [
+        'js/editor/hallo-supsub-plugin.js',
+    ]
+    js_includes = format_html_join('\n', '<script src="{0}{1}"></script>',
+                                   ((settings.STATIC_URL, filename) for filename in js_files)
+                                   )
+    return js_includes + \
+        """
         <script>
-            halloPlugins = {
-        'halloformat': {},
-        //'halloheadings': {formatBlocks: []},
-        'hallolists': {},
-        // 'hallohr': {},
-        'halloreundo': {},
-        'hallolink': {},
-        'hallowagtaillink': {},
-        'hallorequireparagraphs': {}
+
+         halloPlugins = {
+            'halloformat': {},
+            'hallolists': {},
+            'halloreundo': {},
+            'hallolink': {},
+            'hallowagtaillink': {},
+            'hallorequireparagraphs': {}
         };
+
+        registerHalloPlugin('hallosupsub');
+
         </script>
     """
+
+
+@hooks.register('insert_editor_css')
+def editor_css():
+    return format_html('<link rel="stylesheet" href="'
+                       + settings.STATIC_URL
+                       + 'css/font-awesome.min.css">')
