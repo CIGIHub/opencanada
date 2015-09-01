970638b53ecef03f472bea510064c61183248fdd
Albert O'Connor <albert@albertoconnor.ca>
2015-09-01 14:38:49 -0400

    Fix test to support string changes in python 3

--- core/tests.py
+++ core/tests.py
@@ -118,4 +118,4 @@ class HomePageTestCase(TestCase):
         resp = client.get('/feed/')
 
         self.assertEqual(resp.status_code, 200)
-        self.assertIn('<title>In Depth Articles - No Topic - Not Live</title>', resp.content)
+        self.assertIn(b'<title>In Depth Articles - No Topic - Not Live</title>', resp.content)
