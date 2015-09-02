4bc54b33a1a7c9844205f9ccc1cc12a6465dd2c1
Mike Sage <mike@sagesolutionsinc.ca>
2015-09-02 11:47:33 -0400

    OCW-111 OCW-280 faded in related content

--- core/static/js/features.js
+++ core/static/js/features.js
@@ -57,6 +57,27 @@ var FeatureStyles = FeatureStyles || {
                     }
                 });
             }
-        }
+        },
+        RelatedArticles: {
+            initialize: function () {
+                
+                /* Every time the window is scrolled ... */
+                $(window).scroll( function(){
+                
+                    /* Check the location of each desired element */
+                    $('.related-articles .row > div').each( function(i){
+                        
+                        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                        var bottom_of_window = $(window).scrollTop() + $(window).height();
+                        
+                        /* If the object is completely visible in the window, fade it it */
+                        if( bottom_of_window > bottom_of_object ){  
+                            $(this).animate({'opacity':'1'},400);        
+                        }   
+                    }); 
+                });
+
+            }
+        },
 
     };
