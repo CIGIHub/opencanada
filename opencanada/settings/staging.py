dbdad09a201838384f19542b73e509c92599c3fa
Albert O'Connor <albert@albertoconnor.ca>
2015-09-23 16:13:13 -0400

    Enable the admin on staging.

--- opencanada/settings/staging.py
+++ opencanada/settings/staging.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, unicode_literals
 
 from .production import *
 
+ROOT_URLCONF = 'opencanada.urls_admin'
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
