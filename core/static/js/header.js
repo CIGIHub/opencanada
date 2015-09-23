a30940c6dcccf87c23f38cfba9c1350a4eac2b2d
Mike Sage <mike@sagesolutionsinc.ca>
2015-09-23 15:25:48 -0400

    updated comments, removed commented out code

--- core/static/js/header.js
+++ core/static/js/header.js
@@ -1,6 +1,6 @@
 var Header = Header || {
         Structure: {
-            //toggle Banner heading based on window width and page types. This function should only get called when the user scrolls the page.
+            //toggle Banner heading based on window width and page types. This function should only get called when the user scrolls the page and on page load.
             toggleHeading: function () {
 
                 /* There are many ways the header needs to be altered after loading
@@ -14,8 +14,6 @@ var Header = Header || {
                 */
                 var offset = Math.max($('header').height(), $('.jumbotron.main-feature').height());
 
-                //var bodyTag = $('body');
-
                 if ($(document).scrollTop() >= offset) {
                     collapseHeaderOn();
                 } else {
@@ -24,11 +22,9 @@ var Header = Header || {
 
                 if ($('header').hasClass('collapsed')) {
                     Header.Positioning.transparencyOff();
-                }else if ($('.jumbotron').length && !$("body").hasClass("template-home-page")) {
+                } else if ($('.jumbotron').length && !$("body").hasClass("template-home-page")) {
                      Header.Positioning.transparencyOn();
                 }
-
-                
 
                 function collapseHeaderOn() {
                     if ($('#article-page').length) {
@@ -41,7 +37,6 @@ var Header = Header || {
                 }
 
                 function collapseHeaderOff() {
-                    
                     if ($('#article-page').length) {
                         $('header').removeClass('article-scroll');
                     } else {
@@ -50,108 +45,6 @@ var Header = Header || {
 
                     $('header').removeClass('collapsed');
                 }
-
-                /*function fullHeader() {
-                    $('header').removeClass('collapsed');
-                    $('.toggle-mobile').hide();
-                    //$('nav').removeClass('mobile-menu open');
-                    bodyTag.removeClass('article-scroll');
-
-                    if ($('.template-home-page').length) {
-                        $('.tagline').show();
-                    }
-                }
-
-                function collapsedHeader() {
-
-                    $('header').addClass('collapsed');
-                    if (!$('header').hasClass("transparency")) {
-                        $('.toggle-mobile').show();
-                    }
-
-                    //$('nav').addClass('mobile-menu');
-
-                    if ($('#article-page').length && bodyTag.hasClass('article-scroll') && $(window).width() <= breakpoint) {
-                        bodyTag.removeClass('article-scroll');
-                    }
-                    if ($('.template-home-page').length) {
-                        $('.tagline').hide();
-                    }
-                }
-
-                function fullScroll() {
-                    $('header, #search-box').toggleClass('collapsed scrolled', $(document).scrollTop() >= offset);
-
-                    if ($('header').hasClass('scrolled')) {
-                        Header.Positioning.transparencyOff();
-                    }else if ($('.jumbotron').length && !$("body").hasClass("template-home-page")) {
-                         Header.Positioning.transparencyOn();
-                    }
-
-                    var headerRow = $('header .header-row');
-                    if ($('header').hasClass('collapsed')) {
-                        collapsedHeader();
-
-                        if ($('#article-page').length) {
-                            bodyTag.addClass('article-scroll');
-                            headerRow.removeClass('col-md-4');
-                        }
-                    }
-                    else {
-                        if (!(headerRow.hasClass('col-md-4'))) {
-                            headerRow.addClass('col-md-4');
-                        }
-                        fullHeader();
-
-                    }
-                }
-
-                function collapsedScroll() {
-                    collapsedHeader();
-
-                    $('header').toggleClass('scrolled', $(document).scrollTop() >= offset);
-                    if ($('header').hasClass('scrolled')) {
-                        Header.Positioning.transparencyOff();
-                    }else if ($('.jumbotron').length && !$("body").hasClass("template-home-page")) {
-                         Header.Positioning.transparencyOn();
-                    }
-
-                }
-
-                if ($(window).width() >= breakpoint) {
-
-                    if (bodyTag.hasClass('small-article')) {
-                        bodyTag.removeClass('small-article');
-                    }
-
-                    if ($(document).scrollTop() >= offset) {
-                        if ($('#article-page').length) {
-                            bodyTag.addClass('article-scroll');
-                        }
-                        collapsedHeader();
-                    }
-                    else {
-                        fullHeader();
-                    }
-
-                    $(window).off("scroll touchmove", collapsedScroll);
-                    $(window).on("scroll touchmove", fullScroll);
-
-                }
-                else {
-                    var articlePage = $('#article-page');
-                    if (articlePage.length && bodyTag.hasClass('article-scroll')) {
-                        bodyTag.removeClass('article-scroll');
-                    }
-                    if (articlePage.length) {
-                        bodyTag.addClass('small-article');
-                    }
-                    collapsedHeader();
-
-                    $(window).off("scroll touchmove", fullScroll);
-                    $(window).on("scroll touchmove", collapsedScroll);
-
-                }*/
             }
         }, 
         Positioning: {
