526b0ba397c07384119ef14e5142d98663a72f84
Caroline Simpson <csimpson@cigionline.org>
2015-09-23 16:26:03 -0400

    Use redis on staging.

--- opencanada/settings/staging.py
+++ opencanada/settings/staging.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import, unicode_literals
 
 from .production import *
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#     }
+# }
 
 IS_PRODUCTION = False
