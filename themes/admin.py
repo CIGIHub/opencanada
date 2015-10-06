61010a3c25810d0a8aae2197c78d9f1eabc82acb
Caroline Simpson <csimpson@cigionline.org>
2015-10-06 17:38:12 -0400

    Removing menu editing capabilities.

--- themes/admin.py
+++ themes/admin.py
@@ -7,7 +7,5 @@ from . import models
 admin.site.register(models.LogoBlock)
 admin.site.register(models.TextBlock)
 admin.site.register(models.FollowLink)
-admin.site.register(models.Menu)
-admin.site.register(models.MenuItem)
 admin.site.register(models.ThemeContent)
 admin.site.register(models.Theme)
