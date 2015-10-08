84df91c737a3b55250010ce5d7b61994cf52540f
Mike Sage <mike@sagesolutionsinc.ca>
2015-10-08 14:46:24 -0400

    updated oilsands page OCW-663

--- themes/static/themes/oilsands/js/script.js
+++ themes/static/themes/oilsands/js/script.js
@@ -24,14 +24,13 @@ jQuery(document).ready(function() {
     }
 
     //update the height of the main feature depending on the screen size.
-    //since we know the bg image is 1300 x 1175, we can always show the same percentage of the image
+    //since we know the bg image is 1276 x 1020, we can always show the same percentage of the image
     //no matter what screen size we are on.
     function updateMainFeature(){
 
         var width = $(window).width();
-        height = width * 1175 / 1300;
-        height = Math.min( 1000, Math.max( 550, height * 0.85)); //85% so there is some overlap into the main content
-
+        height = width * 1020 / 1276;
+        height = height * 0.85; //85% so there is some overlap into the main content
         $('.main-feature').css("height", height + "px");
     }
 
