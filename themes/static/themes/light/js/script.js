165bbfbe12eadb73b2b84ba0548d03fd30a2f7b6
Mike Sage <mike@sagesolutionsinc.ca>
2015-10-15 15:15:14 -0400

    OCW-699 merge issues

--- themes/static/themes/light/js/script.js
+++ themes/static/themes/light/js/script.js
@@ -1,47 +1,20 @@
 jQuery(document).ready(function() {
 
-    updateHeaderTransparency();
-
-    $(window).scroll(function(){
-	    updateHeaderTransparency();
-	});
+    updateMainFeature();
 
     $(window).resize(function(){
-        updateHeaderTransparency();
+        updateMainFeature();
     });
+    
+    //update the height of the main feature depending on the screen size.
+    //since we know the bg image is 1276 x 1020, we can always show the same percentage of the image
+    //no matter what screen size we are on.
+    function updateMainFeature(){
 
-    function updateHeaderTransparency() {
-        var offset = ($('header').height() + parseInt($('main').css('padding-top')) ) * -1;
-        $('.main-feature').css("margin-top", offset);
-
-    	if ($('header').hasClass('scrolled') || $('#search-box').hasClass("open")) {
-	        transparencyOff();
-	    } else if ($('.main-feature').length && !$("body").hasClass("template-home-page")) {
-	         transparencyOn();
-	    }
+        var width = $(window).width();
+        height = width * 1020 / 1276;
+        height = height * 0.85; //85% so there is some overlap into the main content
+        $('.main-feature').css("height", height + "px");
     }
 
-    function transparencyOn() {
-        $('header').addClass("transparency");
-        $('#toggle-mobile').hide();
-        $('#search-box-toggle').hide();
-        $('#main-menu').hide();
-    }
-
-    function transparencyOff() {
-        $('header').removeClass("transparency");
-        $('#toggle-mobile').fadeIn(1000);
-        $('#search-box-toggle').fadeIn(1000);
-        $('#main-menu').show();
-        //$('.wordmark').css("opacity",1);
-
-        if ($('#search-box').hasClass("open") && $(window).width() >= breakpoint) {
-
-            if ($('header').hasClass('collapsed')) {
-                $('#toggle-mobile').show();
-            } else {
-               $('#toggle-mobile').hide();
-            }
-        }
-    }
 });
