e3d85062dd88452b9238c04eea9aa1a7c6ae1ea5
Mike Sage <mike@sagesolutionsinc.ca>
2015-10-15 14:26:13 -0400

    OCW-699 refactored header transparency so I can use it in another theme

--- themes/static/themes/light/js/script.js
+++ themes/static/themes/light/js/script.js
@@ -1,10 +1,8 @@
 jQuery(document).ready(function() {
 
-    updateHeaderTransparency();
 
-    $(window).scroll(function(){
-	    updateHeaderTransparency();
-	});
+    updateHeaderTransparency();
+    updateMainFeature();
 
     $(window).resize(function(){
         updateHeaderTransparency();
@@ -44,4 +42,19 @@ jQuery(document).ready(function() {
             }
         }
     }
+
+        updateMainFeature();
+    });
+
+    //update the height of the main feature depending on the screen size.
+    //since we know the bg image is 1276 x 1020, we can always show the same percentage of the image
+    //no matter what screen size we are on.
+    function updateMainFeature(){
+
+        var width = $(window).width();
+        height = width * 1020 / 1276;
+        height = height * 0.85; //85% so there is some overlap into the main content
+        $('.main-feature').css("height", height + "px");
+    }
+
 });
