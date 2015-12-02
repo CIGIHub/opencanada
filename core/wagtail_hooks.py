1484e59a606e8cca6c78d8c3080a60c2a70d8d9b
Albert O'Connor <albert@albertoconnor.ca>
2015-12-02 11:45:50 -0500

    Add admin link to run the update_index management command.

--- core/wagtail_hooks.py
+++ core/wagtail_hooks.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import
 
 from django.conf import settings
+from django.conf.urls import url
+from django.contrib import messages
+from django.core import management
 from django.core.urlresolvers import reverse
+from django.shortcuts import redirect, render
 from django.utils.html import format_html, format_html_join
+from wagtail.wagtailadmin.menu import MenuItem
 from wagtail.wagtailcore import hooks
 from wagtail.wagtailcore.whitelist import attribute_rule
 
@@ -59,3 +64,27 @@ class LogoutLinkItem(object):
 @hooks.register('construct_wagtail_userbar')
 def add_logout_link_item(request, items):
     return items.append(LogoutLinkItem())
+
+
+def rebuild_search_index_view(request):
+    if request.method == 'POST':
+        management.call_command('update_index')
+        messages.info(request, "The search index was rebuilt")
+        return redirect('wagtailadmin_home')
+
+    return render(
+        request,
+        'wagtailadmin/search/rebuild.html'
+    )
+
+
+@hooks.register('register_admin_urls')
+def urlconf_time():
+    return [
+        url(r'^rebuild_search/$', rebuild_search_index_view, name='admin_rebuild_search'),
+    ]
+
+
+@hooks.register('register_admin_menu_item')
+def register_frank_menu_item():
+    return MenuItem('Rebuild Search', reverse('admin_rebuild_search'), classnames='icon icon-tag', order=10000)
