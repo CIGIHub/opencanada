53d337ee7124ccb9bf919d20bb8f0dba1181bee7
Albert O'Connor <albert@albertoconnor.ca>
2015-12-02 23:35:00 -0500

    Last resort, see if changing the wagtail migrations fixes the problem...

--- opencanada/settings/test.py
+++ opencanada/settings/test.py
@@ -6,15 +6,3 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
-# https://gist.github.com/NotSqrt/5f3c76cd15e40ef62d09
-class DisableMigrations(object):
-
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return "notmigrations"
-
-MIGRATION_MODULES = DisableMigrations()
