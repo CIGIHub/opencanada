cee453e2b532ddc30b6659d6cbb346285a31efb4
Alexander Myciak <amyciak@carolines-mbp.ciginet.pvt>
2016-04-12 12:57:24 -0400

    OCW-792 #comment Fixed image manipulation functions so that functionality is preserved. Note that circle-cropping will only work with PNG files. There is more work to do to remove this Filter and use CSS instead. We should only be using these functions on Newsletters.

--- images/image_operations.py
+++ images/image_operations.py
@@ -1,29 +1,45 @@
 from PIL import Image, ImageDraw
 from wagtail.wagtailimages.image_operations import FillOperation
+from willow.plugins.pillow import PillowImage
 
 
-class CircleOperation(FillOperation):
+class AlphaOperation(FillOperation):
+    def putalpha(self, willow, alpha):
+        willow.image.putalpha(alpha)
+        # willow.image.save('alpha.png')
+        return PillowImage(willow.image)
+
+
+class DrawBitmapOperation(AlphaOperation):
+    def draw_bitmap(self, willow, bitmap, fill):
+        draw = ImageDraw.Draw(willow.image)
+        draw.bitmap((0, 0), bitmap, fill)
+        del draw
+        # willow.image.save('bitmap.png')
+        return PillowImage(willow.image)
+
+
+class CircleOperation(AlphaOperation):
     def run(self, willow, image):
         if image.width < self.width or image.height < self.height:
             # unable to process image at all since the putalpha will fail
             return
 
-        super(CircleOperation, self).run(willow, image)
+        willow = super(CircleOperation, self).run(willow, image)
 
         mask_size = (3 * self.width, 3 * self.height)
         mask = Image.new('L', mask_size, 0)
         draw = ImageDraw.Draw(mask)
         draw.ellipse((0, 0) + mask_size, fill=255)
+        del draw
         mask = mask.resize((self.width, self.height), Image.ANTIALIAS)
 
-        willow.putalpha(mask)
-        willow.original_format = 'png'
+        willow = self.putalpha(willow, mask)
+        return willow
 
 
-class CircleWithRingOperation(FillOperation):
-
+class CircleWithRingOperation(DrawBitmapOperation):
     def construct(self, size, *extra):
-
         self.circle_color = (204, 204, 204, 1)
         self.ring_width = 1
 
@@ -56,8 +72,7 @@ class CircleWithRingOperation(FillOperation):
             # unable to process image at all since the putalpha will fail
             return
 
-        super(CircleWithRingOperation, self).run(willow, image)
-        willow.original_format = 'png'
+        willow = super(CircleWithRingOperation, self).run(willow, image)
 
         ring_width = 3 * self.ring_width
         mask_size = (3 * self.width, 3 * self.height)
@@ -66,14 +81,21 @@ class CircleWithRingOperation(FillOperation):
         draw.ellipse((0, 0) + mask_size, fill=255)
         draw.ellipse((ring_width, ring_width) + (3 * self.width - ring_width, 3 * self.height - ring_width), fill=0)
         mask = mask.resize((self.width, self.height), Image.ANTIALIAS)
+        # mask.save('circle-ring1.png')
+        ring_mask = mask
         del draw
 
-        willow.draw_bitmap(mask, self.circle_color)
+        willow = self.draw_bitmap(willow, ring_mask, self.circle_color)
 
         mask = Image.new('L', mask_size, 0)
         draw = ImageDraw.Draw(mask)
         draw.ellipse((0, 0) + mask_size, fill=255)
         mask = mask.resize((self.width, self.height), Image.ANTIALIAS)
+        # mask.save('circle-ring2.png')
         del draw
 
-        willow.putalpha(mask)
+        mask.paste(ring_mask, (0, 0), ring_mask)
+        # mask.save('pasted.png')
+
+        willow = self.putalpha(willow, mask)
+        return willow
--- images/wagtail_hooks.py
+++ images/wagtail_hooks.py
@@ -2,9 +2,7 @@ from __future__ import absolute_import
 
 import logging
 
-from PIL import ImageDraw
 from wagtail.wagtailcore import hooks
-from willow.backends.pillow import PillowBackend
 
 from . import image_operations
 
@@ -17,19 +15,3 @@ def register_image_operations():
         ('circle', image_operations.CircleOperation),
         ('rcircle', image_operations.CircleWithRingOperation),
     ]
-
-
-@PillowBackend.register_operation('putalpha')
-def putalpha(backend, alpha):
-    try:
-        backend.image.putalpha(alpha)
-    except ValueError as e:
-        # Log exception but return and don't apply the operation.
-        logger.exception(e)
-        return
-
-
-@PillowBackend.register_operation('draw_bitmap')
-def draw_bitmap(backend, bitmap, fill):
-    image = ImageDraw.Draw(backend.image)
-    image.bitmap((0, 0), bitmap, fill)
