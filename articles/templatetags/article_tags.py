fb22384a7b7ba81ca5f254233551de37e0f673fe
Alexander Myciak <amyciak@carolines-mbp.ciginet.pvt>
2016-04-19 11:32:04 -0400

    millennial #comment Added fields to ChapterBodyBlock and a template tag to format a tweet for sharing.

--- articles/templatetags/article_tags.py
+++ articles/templatetags/article_tags.py
@@ -1,6 +1,8 @@
 import json
+import urllib
 
 from django import template
+from django.utils.text import slugify
 
 from articles.models import (ArticlePage, ExternalArticlePage, SeriesPage,
                              TopicListPage)
@@ -105,3 +107,68 @@ def get_json_data(context):
         if article.json_file:
             json_object = json.load(article.json_file)
             return json_object
+
+@register.filter()
+def romanize(value):
+
+    try:
+        value = int(value)
+    except ValueError:
+        raise TypeError("expected integer, got {}".format(type(value)))
+    if not (0 < value < 4000):
+        raise ValueError("Argument must be between 1 and 3999")
+
+    number_map = (('m', 1000),
+                  ('cm', 900),
+                  ('d', 500),
+                  ('cd', 400),
+                  ('c', 100),
+                  ('xc', 90),
+                  ('l', 50),
+                  ('xl', 40),
+                  ('x', 10),
+                  ('ix', 9),
+                  ('v', 5),
+                  ('iv', 4),
+                  ('i', 1),
+                  )
+    result = ""
+    for numeral, integer in number_map:
+        while value >= integer:
+            result += numeral
+            value -= integer
+
+    return result
+
+# Should only be called from a page template not a block template since we expect the context to contain the article
+@register.simple_tag(takes_context=True)
+def get_twitter_share_url(context, chapter):
+    try:
+        page = context["self"]
+        article = page.articlepage
+        block = chapter.value
+        chapter_heading = block['heading']
+        tweet_text = block['tweet_text']
+    except ArticlePage.DoesNotExist:
+        twitter_share_url = ''
+    else:
+        twitter_share_params = []
+        url_anchor = '{0}#{1}'.format(article.full_url[:-1], slugify(chapter_heading))
+        text = []
+        hashtags = set([])
+        tweet_text_parts = tweet_text.split(' ')
+        for word in tweet_text_parts:
+            if word[0] == '#':
+                word = word[1:]
+                hashtags.add(word)
+            text.append(word)
+        if len(text) > 0:
+            text = ' '.join(text)
+            twitter_share_params.append('text={0}'.format(urllib.quote_plus(text)))
+        if len(hashtags) > 0:
+            hashtags = ','.join(hashtags)
+            twitter_share_params.append('hashtags={0}'.format(urllib.quote_plus(hashtags)))
+        twitter_share_params.append('url={0}'.format(urllib.quote_plus(url_anchor)))
+        twitter_share_params = '&amp;'.join(twitter_share_params)
+        twitter_share_url = 'https://twitter.com/share?{0}'.format(twitter_share_params)
+    return twitter_share_url
