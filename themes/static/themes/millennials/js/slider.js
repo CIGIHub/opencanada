0fa6aba75cfd8cb8e2fbcb700d2272fb30409996
Natasha Scott <nscott@cigionline.org>
2016-04-28 17:24:14 -0400

    basic pager for non-mobile page working

--- themes/static/themes/millennials/js/slider.js
+++ themes/static/themes/millennials/js/slider.js
@@ -1,78 +1,136 @@
 jQuery(document).ready(function() {
 
-    $('.slider .chapter').hide();
-    $('.slider .chapter').first().addClass('first');
-    $('.slider .chapter').last().addClass('last');
+    var chapter = $('.slider .chapter');
+    var menuItem = $('#toc li a');
+    var pagerItem = $('#nav li a');
+    var pagerListItem = $('#nav li');
 
-    var slides = $('.slider .chapter').length;
     var hash = window.location.hash;
+    var people = ($('#nav ul li').length) - 1;
+    var menuIndex = 0;
+
+    initPage();
+
+    function initPage(){
+        pagerListItem.hide();
+        chapter.hide();
+
+        if(hash === '' || hash === '#undefined'){
+            hash = menuItem.first().attr('href');
+        }
+        loadSlide(hash);
+        setPager(hash);
+    }
+
+    function getSlide(){
+        hash = $(this).attr('href');
+        loadSlide(hash);
+        setPager(hash);
+    }
 
     function loadSlide(hash){
-
-        $('.slider .chapter').removeClass('active').hide();
-        $('.slider .chapter' + hash).addClass('active').fadeIn(600);
-        return false;
+        menuIndex = menuItem.index(this);
         window.location.hash = hash;
+        $('.slider .chapter').removeClass('active').hide();
+        $('.slider .chapter' + hash).addClass('active').fadeIn(800);
+        return false;
     }
 
-    function changePager(hash){
-        var index = $('.slider .chapter' + hash).index()
-        $('.next').removeClass('inactive');
-        $('.prev').removeClass('inactive');
+    function setPager(hash){
+        var selected = null;
+        var position = null;
+        pagerListItem.hide();
+        pagerListItem.removeClass('active');
 
-        if(index == slides - 1){
-            $('.next').addClass('inactive');
+        pagerListItem.each(function(index){
+
+           if($(this).children().attr('href') === hash){
+               $(this).addClass('active').show();
+               selected = $(this);
+               position = index;
+           }
+        });
+
+        // console.log(selected);
+        // console.log(position);
+
+        //set pager for 5 items
+        var left = 2;
+        var right = 2;
+
+        if(position < 2){
+            left = position;
+            right = 4 - position;
         }
-        if(index == 0 ){
-            $('.prev').addClass('inactive');
+        else if(position > (people - 2)){
+            right = people - position;
+            left = 4 - right;
+
         }
+
+        $(selected).nextAll(':lt(' + right + ')').show();
+        $(selected).prevAll(':lt(' + left + ')').show();
+
     }
+
+    function getPagerPosition(){
+        var position = null;
+
+        pagerListItem.each(function(index){
+           if($(this).hasClass('active')){
+               hash = $(this).children().attr('href');
+               position = index;
+           }
+        });
+
+        return {
+            pagerHash : hash,
+            pagerIndex : position
+        };
+    }
+
+    menuItem.on('click tap', getSlide);
+    pagerItem.on('click tap', getSlide);
+
 
     $('.next').click(function(e){
 
-        if(!$('.next').hasClass('inactive')){
-            $('.slider li.active').fadeOut(400, function(){
-                $('.slider li.active').removeClass('active').next().addClass('active');
-                $('.slider li.active').fadeIn(400);
-                var handle = $('.slider li.active').attr('id');
-                var selected = '#' + handle;
-                changePager(selected);
-                window.location.hash = handle;
-            });
+        if(!($('.next').hasClass('inactive'))){
+            var currentHash = getPagerPosition().pagerHash;
+            var pagerPosition = getPagerPosition().pagerIndex;
+            var nextPosition = pagerPosition + 1;
+            var newHash = $('#nav li:eq(' + nextPosition + ')').children().attr('href');
 
-        }
-    });
+            loadSlide(newHash);
+            setPager(newHash);
 
-
-    $('.prev').click(function(e){
-        if(!$('.prev').hasClass('inactive')){
-            $('.slider li.active').fadeOut(400, function(){
-                $('.slider li.active').removeClass('active').prev().addClass('active');
-                $('.slider li.active').fadeIn(400);
-                var handle = $('.slider li.active').attr('id');
-                var selected = '#' + handle;
-                changePager(selected);
-                window.location.hash = handle;
-            });
-
+            if(nextPosition === people){
+                $('.next').addClass('inactive');
+            }
         }
 
     });
 
-    if(hash.length && hash != 'toc' && hash != 'chapter') {
-        loadSlide(hash);
-    }
-    else{
-        $('.slider .chapter').first().addClass('active');
-        $('.slider .chapter').first().fadeIn(400);
-    }
+     $('.prev').click(function(e){
 
-    $(".faces li a").on('click focus', function(e) {
-        e.preventDefault();
-        hash = $(this).attr("href");
-        loadSlide(hash);
+        if(!($('.prev').hasClass('inactive'))){
+            var currentHash = getPagerPosition().pagerHash;
+            var pagerPosition = getPagerPosition().pagerIndex;
+            var nextPosition = pagerPosition - 1;
+            var newHash = $('#nav li:eq(' + nextPosition + ')').children().attr('href');
+
+            loadSlide(newHash);
+            setPager(newHash);
+
+            if(nextPosition === 0){
+                $('.prev').addClass('inactive');
+            }
+        }
 
     });
+
+
+
 
     // function scrollView() {
     //   return this.each(function () {
