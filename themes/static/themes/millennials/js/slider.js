1e66cc26c921d29d81fbff75f9db1271cf3eecc3
Natasha Scott <nscott@cigionline.org>
2016-05-06 16:36:36 -0400

    reworked slider

--- themes/static/themes/millennials/js/slider.js
+++ themes/static/themes/millennials/js/slider.js
@@ -2,26 +2,44 @@
 
     var breakpoint = 985;
     var xsBreakpoint = 600;
-    var chapter = $('.slider .chapter');
-    var menuItem = $('#toc li a');
-    var pagerItem = $('#nav li a');
-    var pagerListItem = $('#nav li');
-
-    var hash = window.location.hash;
-    var people = ($('#nav ul li').length) - 1;
-    var menuIndex = 0;
     var windowWidth = $(window).width();
 
-    function initPage(){
-        pagerListItem.hide();
+    var chapter = $('.profile .chapter');
+    var menuItem = $('#toc li a');
+
+    var pager = $('#pager ul');
+    var pagerItem = $('#pager ul li');
+    var pagerItemLink = $('#pager ul li a');
+    var pagerCount = pagerItem.length;
+    var pagerItemWidth = pagerItem.width();
+    var pagerWidth = pagerItemWidth * 5;
+
+    var hash = window.location.hash;
+    var menuIndex = 0;
+
+    function initPage(windowWidth){
         chapter.hide();
+
+        if(windowWidth < xsBreakpoint){
+            pagerWidth = pagerItemWidth;
+        }
+        else if(windowWidth < breakpoint){
+            pagerWidth = pagerItemWidth * 3;
+        }
+        else{
+            pagerWidth = pagerItemWidth * 5;
+        }
+        $('#pager').css("width",  + pagerWidth + "px");
+        pager.width(pagerCount * pagerItemWidth);
+        pager.css("left", "0px");
 
         if(hash === '' || hash === '#undefined'){
             hash = menuItem.first().attr('href');
             $('.prev').addClass('inactive');
             $('.next').removeClass('inactive');
         }
-        $('.slider .chapter' + hash).addClass('active').fadeIn(800);
+
+        $('.profile .chapter' + hash).addClass('active').fadeIn(800);
         setPager(hash);
     }
 
@@ -35,65 +53,29 @@
     function loadSlide(hash){
         menuIndex = menuItem.index(this);
         window.location.hash = hash;
-        $('.slider .chapter').removeClass('active').hide();
-        $('.slider .chapter' + hash).addClass('active').fadeIn(800);
+        $('.profile .chapter').removeClass('active').hide();
+        $('.profile .chapter' + hash).addClass('active').fadeIn(800);
         return false;
     }
 
     function setPager(hash){
         var selected = null;
-        var position = null;
-        var left = 2;
-        var right = 2;
 
-        if(windowWidth < xsBreakpoint){
-            left = right = 0;
-        }
-        else if(windowWidth < breakpoint){
-            if(position < 1){
-                left = 0;
-                right = 2;
-            }
-            else if(position === people){
-                right = 0;
-                left = 2;
-            }
-            else{
-                left = 1;
-                right = 1;
-            }
-        }
-        else if (windowWidth >= breakpoint){
-            if(position < 2){
-            left = position;
-            right = 4 - position;
-            }
-            else if(position > (people - 2)){
-                right = people - position;
-                left = 4 - right;
-            }
-        }
+        pagerItem.removeClass('selected');
 
-        pagerListItem.hide();
-        pagerListItem.removeClass('selected').removeClass('active-slide');
-
-        pagerListItem.each(function(index){
+        pagerItem.each(function(index){
            if($(this).children().attr('href') === hash){
                $(this).addClass('selected');
                selected = $(this);
                position = index;
            }
         });
-
-        $(selected).nextAll(':lt(' + right + ')').addClass('active-slide');
-        $(selected).prevAll(':lt(' + left + ')').addClass('active-slide');
-
     }
 
     function getPagerPosition(){
         var position = null;
 
-        pagerListItem.each(function(index){
+        pagerItem.each(function(index){
            if($(this).hasClass('selected')){
                hash = $(this).children().attr('href');
                position = index;
@@ -108,57 +90,48 @@
 
     function scrollView() {
       $('html,body').animate({
-          scrollTop: $('.slider').offset().top
+          scrollTop: $('.profile').offset().top
       }, 1000);
         return false;
     }
 
     /*  Activate selected slider and set pager */
-     $(document).ready(function () {
+     $(document).ready(function (e) {
 
         initPage();
         menuItem.on('click tap', getSlide);
-        pagerItem.on('click tap', getSlide);
+        pagerItemLink.on('click tap', getSlide);
 
-        $('.next').click(function(e){
+
+        $('.prev').click(function () {
+            $('.next').removeClass('inactive');
+
+            if(!($('.prev').hasClass('inactive'))) {
+                $('#pager ul').animate({left: '+=' + pagerItemWidth}, 500);
+                if($('#pager ul').css("left") === "-85px"){
+                    $('.prev').addClass('inactive');
+                }
+            }
+        });
+
+        $('.next').click(function () {
+
             $('.prev').removeClass('inactive');
-
             if(!($('.next').hasClass('inactive'))){
-                var currentHash = getPagerPosition().pagerHash;
-                var pagerPosition = getPagerPosition().pagerIndex;
-                var nextPosition = pagerPosition + 1;
-                var newHash = $('#nav li:eq(' + nextPosition + ')').children().attr('href');
-
-                setPager(newHash);
-
-                if(nextPosition === people){
+                $('#pager ul').animate({left: '-='+pagerItemWidth}, 500);
+                console.log($('#pager ul').css("left"));
+                if($('#pager ul').css("left") === "-255px"){
                     $('.next').addClass('inactive');
                 }
 
             }
         });
 
-         $('.prev').click(function(e){
-             $('.next').removeClass('inactive');
-
-            if(!($('.prev').hasClass('inactive'))){
-                var currentHash = getPagerPosition().pagerHash;
-                var pagerPosition = getPagerPosition().pagerIndex;
-                var nextPosition = pagerPosition - 1;
-                var newHash = $('#nav li:eq(' + nextPosition + ')').children().attr('href');
-
-                setPager(newHash);
-
-                if(nextPosition === 0){
-                    $('.prev').addClass('inactive');
-                }
-            }
-        });
     });
 
     $(window).resize(function() {
         windowWidth = $(window).width();
-        initPage();
+        initPage(windowWidth);
     });
 
  })(jQuery);
