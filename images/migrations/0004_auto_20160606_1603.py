0901bfa138ab88d6d9e7c3e7913ae217bee90f48
Alexander Myciak <amyciak@carolines-mbp.ciginet.pvt>
2016-06-06 14:16:37 -0400

    OCW-813 #comment Upgrade to wagtail 1.5; some fixes required in our CustomWagtailSearchAppConfig.

---
+++ images/migrations/0004_auto_20160606_1603.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import wagtail.wagtailimages.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('images', '0003_auto_20160405_1542'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='attributedrendition',
+            name='file',
+            field=models.ImageField(height_field='height', width_field='width', upload_to=wagtail.wagtailimages.models.get_rendition_upload_to),
+        ),
+    ]
