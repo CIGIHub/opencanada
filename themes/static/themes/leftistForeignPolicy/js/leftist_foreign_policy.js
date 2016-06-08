0ff750dc52f428d9e7698503fe6cbf81e43fba69
Natasha Scott <nscott@cigionline.org>
2016-06-08 11:35:32 -0400

    setting up animation for top of feature

--- themes/static/themes/leftistForeignPolicy/js/leftist_foreign_policy.js
+++ themes/static/themes/leftistForeignPolicy/js/leftist_foreign_policy.js
@@ -1,14 +1,24 @@
 jQuery(document).ready(function() {
     
-    var windowWidth = $(window).width();
-    var featureHeight = $('.leftist-feature').height();
+    var windowWidth;
+    var featureHeight;
 
+    //wrap the work 'leftist' to apply styling
 	$('.leftist-feature h1').html($('.leftist-feature h1').html().replace(/(Leftist)/g,'<span>$1</span>'));
 
     $(window).on('scroll load resize', function(){
     	windowWidth = $(window).width();
-    	featureHeight = $('.leftist-feature').height();
+		
+		//reset feature height on resize
+		$('.leftist-feature').css("height", "auto");
+    	featureHeight = $('.feature-left').height();
+    	$('.leftist-feature').css("height", featureHeight);
+    	$('.feature-right').css("height", "100%");  	
 
+    	/*
+    		for desktop size fix the table of contents to the left at 100px down the page.  On mobile allow the
+    		table of contents to sit inline with the rest of the content.
+    	*/
     	if(windowWidth > 768){
     		if($('body').scrollTop() >= (featureHeight - 100)){
 	    		$('.table-of-contents').css({
@@ -32,8 +42,13 @@ jQuery(document).ready(function() {
 	    			"float": "none"
 	    		});
     	}
-
-    	
+	
+    	if($('body').scrollTop() > (featureHeight/4) ){
+    		$('.feature-right').addClass('slide-left');
+    	}
+    	if($('body').scrollTop() < (100) ){
+    		$('.feature-right').removeClass('slide-left');
+    	}
 
     });
 
