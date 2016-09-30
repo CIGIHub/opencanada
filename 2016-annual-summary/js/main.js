aa801bca39396bb09e6a487ac87ac1bd5747035f
Franklin JE Flitton <hello@frankflitton.com>
2016-09-30 11:16:18 -0400

    Structure + Animation Setup
    Scrolling animations, background images,

--- 2016-annual-summary/js/main.js
+++ 2016-annual-summary/js/main.js
@@ -1,1 +1,10 @@
-
+wow = new WOW(
+    {
+		  boxClass:     'wow',      
+		  animateClass: 'animated', 
+		  offset:       50,          
+		  mobile:       true,       
+		  live:         true
+	}
+)
+wow.init();
