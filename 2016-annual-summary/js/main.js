052afc3f1ff905d9eb4427d4c43f6d338308c9a0
Franklin JE Flitton <hello@frankflitton.com>
2016-10-03 14:34:15 -0400

    Z-Index Cropping
    Less loop + JS counter on sections

--- 2016-annual-summary/js/main.js
+++ 2016-annual-summary/js/main.js
@@ -8,3 +8,19 @@ wow = new WOW(
 	}
 )
 wow.init();
+
+
+var sections = $("body").find("section").length;
+var sections = sections + 10;
+//console.log(sections);
+var zNumber = 0;
+
+$("section").each(function(c) {
+    console.log("this  " + c);
+    $(this).addClass("z-"+[sections-c]);
+//    $(this).addClass("z-"+[c]);
+    
+});
+
+$("img").click(function(e){e.preventDefault();});
+$("div").click(function(e){e.preventDefault();});
