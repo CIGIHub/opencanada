222b3907c22420ae624613688f6b0b2d7cd2614a
Franklin JE Flitton <hello@frankflitton.com>
2016-10-05 12:34:33 -0400

    IE Fallback Stack. Remove Modernizr From Plugins.js. Remove Old JS Code

--- 2016-annual-summary/js/main.js
+++ 2016-annual-summary/js/main.js
@@ -1,30 +1,3 @@
-//wow = new WOW(
-//    {
-//		  boxClass:     'wow',      
-//		  animateClass: 'animated', 
-//		  offset:       0,          
-//		  mobile:       true,       
-//		  live:         true
-//	}
-//)
-//wow.init();
-//
-//$(document).ready(function() {
-//	$('#sticky-navigation').scrollspy();
-//});
-
-
-//var sections = $("body").find("img.cover").length;
-//var sections = sections + 10;
-////console.log(sections);
-//var zNumber = 0;
-//
-//$("img.cover").each(function(c) {
-//    console.log("this  " + c);
-//    $(this).addClass("z-"+[sections-c]);
-////    $(this).addClass("z-"+[c]);
-//});
-
 AOS.init({
   startEvent: 'DOMContentLoaded',
   offset: 200,
@@ -68,7 +41,7 @@ function countScroll(d) {
 			var lastDate = nextDate; 
 		}
 	});
-	var lastDate = nextDate;             // The function returns the product of p1 and p2
+	var lastDate = nextDate;
 }
 
 $(function() {
@@ -108,46 +81,3 @@ $(window).scroll(function(){
 
 
 
-//// 2 Way wowjs http://stackoverflow.com/questions/33187211/wow-js-repeat-animation-every-time-you-scroll-up-or-down
-//
-//$(function() {
-//
-//  var $window           = $(window),
-//      win_height_padded = $window.height() * 1.1,
-//      isTouch           = Modernizr.touch;
-//
-//  if (isTouch) { $('.revealOnScroll').addClass('animated'); }
-//
-//  $window.on('scroll', revealOnScroll);
-//
-//  function revealOnScroll() {
-//    var scrolled = $window.scrollTop(),
-//        win_height_padded = $window.height() * 1.1;
-//
-//    // Showed...
-//    $(".revealOnScroll:not(.animated)").each(function () {
-//      var $this     = $(this),
-//          offsetTop = $this.offset().top;
-//
-//      if (scrolled + win_height_padded > offsetTop) {
-//        if ($this.data('timeout')) {
-//          window.setTimeout(function(){
-//            $this.addClass('animated ' + $this.data('animation'));
-//          }, parseInt($this.data('timeout'),10));
-//        } else {
-//          $this.addClass('animated ' + $this.data('animation'));
-//        }
-//      }
-//    });
-//    // Hidden...
-//   $(".revealOnScroll.animated").each(function (index) {
-//      var $this     = $(this),
-//          offsetTop = $this.offset().top;
-//      if (scrolled + win_height_padded < offsetTop) {
-//        $(this).removeClass('animated fadeInUp flipInX lightSpeedIn')
-//      }
-//    });
-//  }
-//
-//  revealOnScroll();
-//});
