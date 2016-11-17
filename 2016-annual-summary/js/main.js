610718bb37a4e720c0eed11bd222f3e746496309
Franklin JE Flitton <hello@frankflitton.com>
2016-11-17 02:22:55 -0500

    Design Revisions
    Misc Design revisions including: new font, em’s, mobile nav and
    pagination.

--- 2016-annual-summary/js/main.js
+++ 2016-annual-summary/js/main.js
@@ -1,3 +1,18 @@
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 AOS.init({
 //  startEvent: 'DOMContentLoaded',
   offset: 200,
@@ -15,21 +30,39 @@ $(window).load(function() {
 		$('.counter').html( startDate );
 });
 
-$('.counter').bind("DOMSubtreeModified",function(){
+var counterFn = debounce(function() {
 	var dateInput = $(".counter").text();
 	var datePrint = moment(dateInput, "x").format("MMMM DD, YYYY"); // Oct 4th 16; 
 	$('.date').text(datePrint);
-});
+}, 10);
 
+$('.counter').bind("DOMSubtreeModified",counterFn);
+
+var dd;
 function countScroll(d) {
+	
+	console.log(d);
+	
     var nextDate = $("section:nth-of-type("+[d+1]+")").data("date");
     var prevDate = $("section:nth-of-type("+[d+0]+")").data("date");
 						
 	if (d === 1) {
 		var lastDate = startDate;
 	};
+	
+	dd = d - 1;
+	if (dd >= 0) {
+		$('.paging').addClass('show');
+	};	
+	if (dd <= -1) {
+		$('.paging').removeClass('show');
+	};
+	if (dd == -1) {
+		dd = dd+1;
+	};
+	
+	$('.aCurrent').html( dd );
 
-//	alert("next " + nextDate + " | last " + lastDate);
 
 	$('.counter').countTo({
 		from: nextDate,
@@ -37,11 +70,12 @@ function countScroll(d) {
 		speed: scrollingSpeed,
 		refreshInterval: 50,
 		onComplete: function(value) {
-//			console.debug(this);
 			var lastDate = nextDate; 
 		}
 	});
 	var lastDate = nextDate;
+	
+	
 }
 
 $(function() {
@@ -69,7 +103,7 @@ $(function() {
 var topofDiv = $("section:nth-of-type(1)").offset().top; //gets offset of header
 var topofDiv2 = $("section:nth-of-type(3)").offset().top; //gets offset of header
 var hideDate = $("#note").offset().top + $("#note").outerHeight() - 80; //gets offset of header
-var height = $("section:nth-of-type(1)").outerHeight() + 20; //gets height of header
+var height = $("section:nth-of-type(1)").outerHeight() + 0; //gets height of header
 
 $(".dateContainer").hide();
 $(window).scroll(function(){
@@ -82,3 +116,48 @@ $(window).scroll(function(){
 });
 
 
+$('.bgTrigger').each(function() {
+	$( this ).hover(
+  function() {
+    $( this ).closest('section').addClass( "zoomImage" );
+  }, function() {
+    $( this ).closest('section').removeClass( "zoomImage" );
+  });
+});
+
+var n = $( ".story" ).length;
+$('.aTotal').html(n);
+
+$(".aNext").click(function(e) {
+    e.preventDefault();
+    $.scrollify.setOptions({
+        scrollSpeed: 2500,
+    });
+    $.scrollify.next();
+});
+
+$(".aBack").click(function(e) {
+    e.preventDefault();
+    $.scrollify.setOptions({
+        scrollSpeed: 2500,
+    });
+    $.scrollify.previous();
+});
+
+$('section').each(function( i ) {
+	var Title = $( "section:nth-of-type("+[i+1]+")" ).find('h2').text();
+	var navString = "<li><a href='"+ '#' + this.id +"'><span>"+ [i-1] +".</span> <h2>" + Title + "</h2></a></li>";
+	if( i >= 1 ){
+	$( ".mNav-menu ul" ).append( navString );
+	}
+});
+
+$( ".mNav" ).click(function() {
+  $( ".mNav-menu" ).toggleClass( "closed" );
+});
+
+$( ".mNav-menu ul a" ).each(function() {
+	$(this).click(function() {
+  		$( ".mNav-menu" ).toggleClass( "closed" );
+	});
+});
