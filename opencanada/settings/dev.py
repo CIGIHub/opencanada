fa4d1d11f464cc9b2fee2e85a735a3d34b40c25d
Albert O'Connor <albert@albertoconnor.ca>
2017-03-01 13:36:33 -0500

    Review DEBUG.

--- opencanada/settings/dev.py
+++ opencanada/settings/dev.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '172.31.9.139']
