4f5aac77d85bf964bd9615e18e970402cd6d7d9f
Albert O'Connor <albert@albertoconnor.ca>
2017-04-05 17:06:20 -0400

    Remove search settings from dev.

--- opencanada/settings/dev.py
+++ opencanada/settings/dev.py
@@ -15,23 +15,6 @@ INSTALLED_APPS = INSTALLED_APPS + (
     'interactives_content',
 )
 
-WAGTAILSEARCH_BACKENDS = {
-    'default': {
-        'BACKEND': 'wagtail.wagtailsearch.backends.elasticsearch2',
-        'INDEX': get_env_variable('ELASTICSEARCH_INDEX'),
-        'TIMEOUT': 5000,
-        # This setting will not work as intended with the ElasticSearch provided by http://www.searchly.com/
-        # 'ATOMIC_REBUILD': True,
-        'HOSTS': [{
-            'host': get_env_variable('ELASTICSEARCH_HOST'),
-            'port': get_env_variable('ELASTICSEARCH_PORT'),
-            'http_auth': (get_env_variable('ELASTICSEARCH_USER'), get_env_variable('ELASTICSEARCH_PASSWORD')),
-            'use_ssl': True,
-            'verify_certs': False,
-        }]
-    },
-}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
