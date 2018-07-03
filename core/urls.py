23264f06f2e177306f0776fd54d2500de2f132e6
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-07-03 13:27:59 -0400

    Fix urls

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from core import views
 
 app_name = 'core'
 
 urlpatterns = [
-    url(r'^share/count/(?P<page_id>\d+)/$', views.social_share_count, name='social_share_count'),
+    re_path(r'^share/count/(?P<page_id>\d+)/$', views.social_share_count, name='social_share_count'),
 ]
