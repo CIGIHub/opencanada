7a2c19f778afc6dd6631579c83c3a11c48c586fb
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-12-10 15:00:12 -0500

    Comment out production configs

--- opencanada/settings/production.py
+++ opencanada/settings/production.py
@@ -13,28 +13,28 @@ ALLOWED_HOSTS = ['*'] # TODO: Fix this
 # http://django-compressor.readthedocs.org/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
 COMPRESS_OFFLINE = True
 
-WAGTAILSEARCH_BACKENDS = {
-    'default': {
-        'BACKEND': 'wagtail.search.backends.elasticsearch2',
-        'INDEX': get_env_variable('ELASTICSEARCH_INDEX'),
-        'TIMEOUT': 5000,
-        # This setting will not work as intended with the ElasticSearch provided by http://www.searchly.com/
-        # 'ATOMIC_REBUILD': True,
-        'HOSTS': [{
-            'host': get_env_variable('ELASTICSEARCH_HOST'),
-            'port': get_env_variable('ELASTICSEARCH_PORT'),
-            'http_auth': (get_env_variable('ELASTICSEARCH_USER'), get_env_variable('ELASTICSEARCH_PASSWORD')),
-            'use_ssl': True,
-            'verify_certs': False,
-        }]
-    },
-}
+# WAGTAILSEARCH_BACKENDS = {
+#     'default': {
+#         'BACKEND': 'wagtail.search.backends.elasticsearch2',
+#         'INDEX': get_env_variable('ELASTICSEARCH_INDEX'),
+#         'TIMEOUT': 5000,
+#         # This setting will not work as intended with the ElasticSearch provided by http://www.searchly.com/
+#         # 'ATOMIC_REBUILD': True,
+#         'HOSTS': [{
+#             'host': get_env_variable('ELASTICSEARCH_HOST'),
+#             'port': get_env_variable('ELASTICSEARCH_PORT'),
+#             'http_auth': (get_env_variable('ELASTICSEARCH_USER'), get_env_variable('ELASTICSEARCH_PASSWORD')),
+#             'use_ssl': True,
+#             'verify_certs': False,
+#         }]
+#     },
+# }
 
-AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
+# AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 COMPRESS_URL = 'https://files.opencanada.org/'
 STATIC_URL = 'https://files.opencanada.org/'
@@ -55,9 +55,9 @@ AWS_S3_CUSTOM_DOMAIN = 'files.opencanada.org'
 #     }
 # }
 
-RAVEN_CONFIG = {
-    'dsn': get_env_variable('RAVEN_DSN'),
-}
+# RAVEN_CONFIG = {
+#     'dsn': get_env_variable('RAVEN_DSN'),
+# }
 
 INSTALLED_APPS = INSTALLED_APPS + (
     'wagtail.contrib.frontend_cache',
@@ -66,26 +66,26 @@ INSTALLED_APPS = INSTALLED_APPS + (
     'caching',
 )
 
-WAGTAILFRONTENDCACHE = {
-    'cloudflare': {
-        'BACKEND': 'wagtail.contrib.frontend_cache.backends.CloudflareBackend',
-        'EMAIL': get_env_variable('CLOUDFLARE_EMAIL'),
-        'TOKEN': get_env_variable('CLOUDFLARE_TOKEN'),
-        'ZONEID': get_env_variable('CLOUDFLARE_ZONE_ID'),
-    },
-}
+# WAGTAILFRONTENDCACHE = {
+#     'cloudflare': {
+#         'BACKEND': 'wagtail.contrib.frontend_cache.backends.CloudflareBackend',
+#         'EMAIL': get_env_variable('CLOUDFLARE_EMAIL'),
+#         'TOKEN': get_env_variable('CLOUDFLARE_TOKEN'),
+#         'ZONEID': get_env_variable('CLOUDFLARE_ZONE_ID'),
+#     },
+# }
 
 FAVICON_PATH = STATIC_URL + 'img/favicon.png'
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': get_env_variable('REDIS_CACHE_ENDPOINT'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': get_env_variable('REDIS_CACHE_ENDPOINT'),
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
 
 IS_PRODUCTION = True
 
