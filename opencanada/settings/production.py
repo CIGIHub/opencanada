580eb6a8093842b80d7475071a25ad642b514591
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-01-11 10:35:43 -0500

    remove compress storage

--- opencanada/settings/production.py
+++ opencanada/settings/production.py
@@ -37,9 +37,9 @@ AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
 #
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 #
-COMPRESS_URL = 'https://heroku-files.opencanada.org/'
+# COMPRESS_URL = 'https://heroku-files.opencanada.org/'
 STATIC_URL = 'https://heroku-files.opencanada.org/'
 MEDIA_URL = 'https://heroku-files.opencanada.org/'
 AWS_S3_CUSTOM_DOMAIN = 'heroku-files.opencanada.org'
