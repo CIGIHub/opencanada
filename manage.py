437dcb5fdc0f895c46a175feb555ae394267992b
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-01-14 09:52:12 -0500

    Fix typo

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ with warnings.catch_warnings():
 if __name__ == "__main__":
     if os.environ.get('PYTHON_ENV') == 'production':
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opencanada.settings.production')
-    elif os.environ.get('PYTHON_ENV') === 'staging':
+    elif os.environ.get('PYTHON_ENV') == 'staging':
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opencanada.settings.staging')
     else:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opencanada.settings')
