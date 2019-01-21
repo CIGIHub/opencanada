3902c46bf7a37657f98005847c6e3155018d1f91
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-01-21 15:37:55 -0500

    Mock staging settings:

--- opencanada/settings/admin.py
+++ opencanada/settings/admin.py
@@ -10,6 +10,20 @@ ROOT_URLCONF = 'opencanada.urls_admin'
 
 PREPEND_WWW = False
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+# Explicitly use database
+WAGTAILSEARCH_BACKENDS = {
+    'default': {
+        'BACKEND': 'wagtail.search.backends.db',
+    }
+}
+
+IS_PRODUCTION = False
 ADMIN_ENABLED = True
 
 SITE_ID = 2
