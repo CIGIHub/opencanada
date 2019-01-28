45482c0ed7e1bbc20bf9e4f1f6695c47dc54c3d2
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-01-28 15:03:15 -0500

    fix: Remove Cloudflare cache invalidator from Wagtail (#681)
    * fix: Remove Cloudflare cache invalidator from Wagtail
    
    * Log response from Cloudflare
    
    * Fix logging statement
    
    * Try print
    
    * Remove print statement

--- caching/invalidate.py
+++ caching/invalidate.py
@@ -4,54 +4,8 @@ import logging
 
 import requests
 from django.conf import settings
-from django.db.models.signals import pre_delete
-from django.dispatch import receiver
-from wagtail.contrib.frontend_cache.utils import purge_page_from_cache
-from wagtail.core.signals import page_published
-
-from articles import models as articles_models
-from core.models import HomePage
-from events.models import EventListPage, EventPage
-from jobs.models import JobPostingListPage, JobPostingPage
-from newsletter.models import NewsletterListPage, NewsletterPage
-from people.models import ContributorListPage, ContributorPage
-from six.moves import xrange
 
 logger = logging.getLogger(__name__)
-
-# for cache invalidation of index pages
-invalidation_map = {
-    JobPostingPage: [JobPostingListPage],
-
-    EventPage: [EventListPage],
-
-    ContributorPage: [ContributorListPage],
-
-    NewsletterPage: [NewsletterListPage],
-
-    articles_models.ArticlePage: [
-        articles_models.ArticleListPage,
-        articles_models.TopicListPage,
-        articles_models.SeriesListPage,
-        HomePage
-    ],
-
-    articles_models.SeriesPage: [
-        articles_models.ArticleListPage,
-        articles_models.TopicListPage,
-        articles_models.SeriesListPage,
-        HomePage,
-    ],
-
-    articles_models.ExternalArticlePage: [
-        articles_models.ArticleListPage,
-        HomePage,
-    ]
-}
-
-
-# TODO: Invalidate related pages when possible.
-
 
 _cloudflare_config = None
 
@@ -109,45 +63,3 @@ def cloudflare_purge_all():
         '/zones/{ZONEID}/purge_cache',
         data,
     )
-
-
-# From http://stackoverflow.com/a/434328/91243
-def chunker(seq, size):
-    return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
-
-
-def cloudflare_purge_urls(urls):
-    if not isinstance(urls, list):
-        urls = [urls]
-
-    for urls in chunker(urls, 30):
-        data = {"files": urls}
-
-        cloudflare_request(
-            requests.delete,
-            '/zones/{ZONEID}/purge_cache',
-            data,
-        )
-
-
-def purge_related(instance):
-    global invalidation_map
-    instance_model = instance.__class__
-    if instance_model not in invalidation_map:
-        return
-    for related_page_model in invalidation_map[instance_model]:
-        for page in related_page_model.objects.live():
-            print("Purging {}".format(page))
-            purge_page_from_cache(page)
-
-
-@receiver(page_published)
-def page_published_handler(instance, **kwargs):
-    cloudflare_purge_all()
-    # purge_related(instance)
-
-
-@receiver(pre_delete)
-def page_deleted_handler(instance, **kwargs):
-    cloudflare_purge_all()
-    # purge_related(instance)
