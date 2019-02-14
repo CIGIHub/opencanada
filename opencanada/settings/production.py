c7a33f243a4a63fbb3313e5f923c361d06d9474a
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-02-14 09:19:51 -0500

    switch back to bonsai

--- opencanada/settings/production.py
+++ opencanada/settings/production.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = [get_env_variable('ALLOWED_HOSTS')]
 WAGTAILSEARCH_BACKENDS = {
     'default': {
         'BACKEND': 'wagtail.search.backends.elasticsearch6',
-        'URLS': [get_env_variable('SEARCHBOX_SSL_URL')],
+        'URLS': [get_env_variable('BONSAI_URL')],
         'INDEX': 'wagtail',
         'TIMEOUT': 5,
         'OPTIONS': {},
