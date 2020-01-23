26d9941e5450d344f68d7058638d74fd9cf60223
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-01-22 23:56:11 -0500

    [chore] Add AWS V4 signature support (#695)

--- opencanada/settings/production.py
+++ opencanada/settings/production.py
@@ -14,6 +14,8 @@ ALLOWED_HOSTS = [get_env_variable('ALLOWED_HOSTS')]
 AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
+S3_USE_SIGV4 = True
+AWS_S3_HOST = get_env_variable('AWS_S3_HOST')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
